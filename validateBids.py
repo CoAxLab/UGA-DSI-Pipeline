ef8bc681876f85e4ba635bfc11087d40cfc3d48b
PETER K <kasatyr@gmail.com>
2025-02-06 22:57:02 -0500

    validate bids reorganisation

--- validateBids.py
+++ validateBids.py
@@ -3,7 +3,7 @@ from bids_validator import BIDSValidator
 
 pipelineDirectory = os.getcwd()
 sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
-sourceDirectoryBids = os.path.join(pipelineDirectory, 'bids')
+sourceDirectoryBids = os.path.join(pipelineDirectory, 'BIDS')
 outDirectoryQC = os.path.join(pipelineDirectory, 'QCOutput')
 try:
     os.mkdir(outDirectoryQC)
@@ -12,11 +12,12 @@ except FileExistsError:
 sifFile = os.path.join(sifDirectory, 'mriqc_latest.sif')
 singularityCommand = f'singularity exec {sifFile}'
 
+BIDSValidator().is_bids(sourceDirectoryBids)
+print(BIDSValidator().is_bids(sourceDirectoryBids))
 
 for subjID in os.listdir(sourceDirectoryBids):
     print(subjID)
     currBidsDir = os.path.join(sourceDirectoryBids, subjID)
+    print(currBidsDir)
     BIDSValidator().is_bids(currBidsDir)
     print(BIDSValidator().is_bids(currBidsDir))
-    #qcCommand = f'{singularityCommand} mriqc {currBidsDir} {outDirectoryQC} participant --participant-label {subjID}'
-    #os.system(qcCommand)
