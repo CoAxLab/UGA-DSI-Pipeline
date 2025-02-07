aa26caac3b4680a4986f3d6ea7367ddef40c3bef
PETER K <kasatyr@gmail.com>
2025-02-06 22:58:26 -0500

    reorg'd qc script for proper BIDS dataset

--- runQC.py
+++ runQC.py
@@ -3,7 +3,7 @@ from bids_validator import BIDSValidator
 
 pipelineDirectory = os.getcwd()
 sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
-sourceDirectoryBids = os.path.join(pipelineDirectory, 'bids')
+sourceDirectoryBids = os.path.join(pipelineDirectory, 'BIDS')
 outDirectoryQC = os.path.join(pipelineDirectory, 'QCOutput')
 try:
     os.mkdir(outDirectoryQC)
@@ -13,22 +13,12 @@ sifFile = os.path.join(sifDirectory, 'mriqc_latest.sif')
 singularityCommand = f'singularity exec {sifFile}'
 
 
-for subjID in os.listdir(sourceDirectoryBids):
-    print(subjID)
-    currBidsDir = os.path.join(sourceDirectoryBids, subjID)
-    print(BIDSValidator().is_bids(currBidsDir))
-    qcCommand = f'{singularityCommand} mriqc {currBidsDir} {outDirectoryQC} participant --participant-label {subjID}'
-    os.system(qcCommand)
-#     if subjID[0:3] != 'sub': 
-#         print(f'skipping {subjID}')
+# for subjID in os.listdir(sourceDirectoryBids):
+#     if subjID in ['participants.tsv', 'dataset_description.json']:
 #         continue
-#     nextDir = os.path.join(sourceDirectoryBids, subjID)
-#     print(BIDSValidator().is_bids(nextDir))
-#     print(nextDir)
-    # try:
-    #     for file in os.listdir(nextDir):
-    #         print(BIDSValidator().is_bids(os.path.join(nextDir, file)))
-    # except NotADirectoryError:
-    #     print('not a directory')
-    
+#    print(subjID)
+#    currBidsDir = os.path.join(sourceDirectoryBids, subjID)
+#print(BIDSValidator().is_bids(currBidsDir))
+qcCommand = f'{singularityCommand} mriqc {sourceDirectoryBids} {outDirectoryQC} participant --participant-label DXSP008'
+os.system(qcCommand)
    
