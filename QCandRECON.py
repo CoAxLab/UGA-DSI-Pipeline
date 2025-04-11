d2d11742bfe29ea98533e600443189771f394a80
PETER K <kasatyr@gmail.com>
2025-04-11 09:33:07 -0400

    error when pipeline is not run, also attempts another possible command for python program path.

--- QCandRECON.py
+++ QCandRECON.py
@@ -7,3 +7,13 @@ if not os.path.isdir(bidsDirectory):
 
 os.system('python runQC.py')
 os.system('python runPipeline.py')
+
+qcOut = os.path.join(pipelineDirectory, 'QCOutput')
+
+if not os.path.isdir(qcOut):
+    print(f'Attempting to run with "python3" command')
+    os.system('python3 runQC.py')
+    os.system('python3 runPipeline.py')
+
+if not os.path.isdir(qcOut):
+    raise Exception(f'ERROR: Pipeline has not run, missing dependencies or Python not on path as "python" or "python3"')
