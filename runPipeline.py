155879dac2e9a9fe2eb266db044124a156a8c52c
PETER K <kasatyr@gmail.com>
2025-04-11 10:56:29 -0400

    separate directory for each subject/session combination

--- runPipeline.py
+++ runPipeline.py
@@ -13,22 +13,17 @@ singularityCommand = f'singularity exec {sifFile}'
 for subjID in os.listdir(bidsDirectory):
     if 'sub-' not in subjID: continue
     subBidsPath = os.path.join(bidsDirectory, subjID)
-    subjectSRCDir = os.path.join(outputDirectorySRC, subjID)
-    try:
-        os.mkdir(subjectSRCDir)
-    except FileExistsError:
-        print(f'\nsrc action already complete for subject: {subjID}!\n')
-        continue
 
     for sesDir in os.listdir(subBidsPath):
-        sesSRCDir = os.path.join(subjectSRCDir, sesDir)
-        os.mkdir(sesSRCDir)
+        subSesTag = f'{subjID}_{sesDir}'
+        subSesSRCDir = os.path.join(outputDirectorySRC, subSesTag)
+        os.mkdir(subSesSRCDir)
         # source files:
         lowStandard = os.path.join(subBidsPath, sesDir, 'dwi' , f'{subjID}_{sesDir}_dir-std_acq-lowb_dwi.nii.gz')
         midStandard = os.path.join(subBidsPath, sesDir, 'dwi' , f'{subjID}_{sesDir}_dir-std_acq-midb_dwi.nii.gz')
         highSandard = os.path.join(subBidsPath, sesDir, 'dwi' , f'{subjID}_{sesDir}_dir-std_acq-highb_dwi.nii.gz')
         # output file:
-        stdOutFile = os.path.join(sesSRCDir, f'{subjID}_{sesDir}_dir-std.src.gz')
+        stdOutFile = os.path.join(subSesSRCDir, f'{subjID}_{sesDir}_dir-std.src.gz')
         srcCommandStandard = f'dsi_studio --action=src --source={lowStandard} --other_source={midStandard},{highSandard} --output={stdOutFile}'
 
         # reversed source files:
@@ -36,7 +31,7 @@ for subjID in os.listdir(bidsDirectory):
         midReverse = os.path.join(subBidsPath, sesDir, 'dwi' , f'{subjID}_{sesDir}_dir-rev_acq-midb_dwi.nii.gz')
         highReverse = os.path.join(subBidsPath, sesDir, 'dwi' , f'{subjID}_{sesDir}_dir-rev_acq-highb_dwi.nii.gz')
         # reversed output file:
-        revOutFile = os.path.join(sesSRCDir, f'{subjID}_{sesDir}_dir-rev.src.gz')
+        revOutFile = os.path.join(subSesSRCDir, f'{subjID}_{sesDir}_dir-rev.src.gz')
 
         srcCommandReversed = f'dsi_studio --action=src --source={lowReverse} --other_source={midReverse},{highReverse} --output={revOutFile}'
 
@@ -53,40 +48,35 @@ for subjID in os.listdir(bidsDirectory):
         print(f'\n{subjID} reversed src exited!\n')
 
 reconOutputDirectory = os.path.join(pipelineDirectory, 'fib')
-for subjID in os.listdir(outputDirectorySRC):
+for subSesID in os.listdir(outputDirectorySRC):
     start = time.time()
-    subInputDir = os.path.join(outputDirectorySRC, subjID)
-    subjRecOutDirectory = os.path.join(reconOutputDirectory, subjID) # fib/subject/
+    srcInputDir = os.path.join(outputDirectorySRC, subSesID)
+    recOutDirectory = os.path.join(reconOutputDirectory, subSesID) # fib/subjectsession/
     try:
-        os.mkdir(subjRecOutDirectory)
+        os.mkdir(recOutDirectory)
     except FileExistsError:
         print(f'\nrecon action already complete for subject: {subjID}.....\n')
         continue
-
-    for ses in os.listdir(subInputDir):
-        currSesSRCDir = os.path.join(subInputDir, ses)
-        subjRecOutDirectory = os.path.join(reconOutputDirectory, subjID) # fib/subject/
         
-        # src files for input
-        for file in os.listdir(currSesSRCDir):
-            if 'rev' in file:
-                revFileName = file
-            elif 'std' in file:
-                stdFileName = file
-        srcFileS = os.path.join(currSesSRCDir, stdFileName)
-        srcFileR = os.path.join(currSesSRCDir, revFileName)
-        fibFileOutput = os.path.join(subjRecOutDirectory, f'{subjID}_{ses}_rec.icbm152_adult.qsdr.1.25.fib.gz')
+    for file in os.listdir(srcInputDir):
+        if 'rev' in file:
+            revFileName = file
+        elif 'std' in file:
+            stdFileName = file
+    srcFileS = os.path.join(srcInputDir, stdFileName)
+    srcFileR = os.path.join(srcInputDir, revFileName)
+    fibFileOutput = os.path.join(recOutDirectory, f'{subSesID}_rec.icbm152_adult.qsdr.1.25.fib.gz')
 
         
 
-        settings = '--method=7 --param0=1.25 --template=0 --qsdr_reso=2.0' # optional settings flags
-        reconCommand = f'dsi_studio --action=rec --source={srcFileS} --rev_pe={srcFileR} --output={fibFileOutput} {settings}'
+    settings = '--method=7 --param0=1.25 --template=0 --qsdr_reso=2.0' # optional settings flags
+    reconCommand = f'dsi_studio --action=rec --source={srcFileS} --rev_pe={srcFileR} --output={fibFileOutput} {settings}'
 
-        fullRecCommand = f'{singularityCommand} {reconCommand}'
-        print(f'\nRunning DSI Studio recon action for subject: {subjID}.....\n')
-        os.chdir(os.path.join(outputDirectorySRC, subjID))
-        print(fullRecCommand)
-        os.system(fullRecCommand)
-        end = time.time()
-        print(f'\n{subjID} recon exited in {end - start} seconds!\n')
-        os.chdir(pipelineDirectory)
+    fullRecCommand = f'{singularityCommand} {reconCommand}'
+    print(f'\nRunning DSI Studio recon action for subject: {subjID}.....\n')
+    os.chdir(srcInputDir)
+    print(fullRecCommand)
+    os.system(fullRecCommand)
+    end = time.time()
+    print(f'\n{subjID} recon exited in {end - start} seconds!\n')
+    os.chdir(pipelineDirectory)
