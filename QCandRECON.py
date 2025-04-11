f4be3193a6290e7be602aa375c1342e51026d8f9
PETER K <kasatyr@gmail.com>
2025-04-11 09:22:31 -0400

    should work. raises exception if BIDS/ dosnt exist.

--- QCandRECON.py
+++ QCandRECON.py
@@ -1,3 +1,9 @@
 import os
 
-os.system 
+pipelineDirectory = os.getcwd()
+bidsDirectory = os.path.join(pipelineDirectory, 'BIDS')
+if not os.path.isdir(bidsDirectory):
+    raise Exception(f'ERROR: Please run the following:\n\tsetupPipeline.py, move data to convertToBids, and run niftiToBids.py\nbefore attempting to run the pipeline!')
+
+os.system('python runQC.py')
+os.system('python runPipeline.py')
