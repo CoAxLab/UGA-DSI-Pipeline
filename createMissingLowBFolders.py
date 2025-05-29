464fba2550ee48eb3f2d10c9fbd6ba195afbb4d0
peter <kasatyr@gmail.com>
2025-05-29 14:10:18 -0400

    init. creates lowBFiles directory if it does not exist (deleted or otherwise)

---
+++ createMissingLowBFolders.py
@@ -0,0 +1,19 @@
+import os
+pipelineDirectory = os.getcwd()
+parentBIDS = os.path.join(pipelineDirectory, 'BIDS')
+lowDir = os.path.join(pipelineDirectory, 'lowBFiles')
+
+try:
+    os.mkdir(lowDir)
+except FileExistsError:
+    print(f'Path:\n\t{lowDir}\nalready exists. Exiting.')
+
+for id in os.listdir(parentBIDS):
+    subLow = os.path.join(lowDir, id)
+    subBIDS = os.path.join(parentBIDS, id)
+    os.mkdir(subLow)
+    for ses in os.listdir(subBIDS):
+        sesLow = os.path.join(subLow, ses)
+        sesBIDS = os.path.join(subBIDS, ses)
+        os.mkdir(sesLow)
+        os.mkdir(os.path.join(sesLow, 'dwi'))
