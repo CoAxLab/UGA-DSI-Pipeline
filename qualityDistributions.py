6004680d47db758fbdcafe1f87a0b837ad6e5c71
Peter <kasatyr@gmail.com>
2025-06-12 22:19:13 -0400

    exports qa metrics

--- qualityDistributions.py
+++ qualityDistributions.py
@@ -3,6 +3,9 @@ import os
 pipelineDirectory = os.getcwd()
 figuresOutput = os.path.join(pipelineDirectory, 'Figures')
 fibDirectory = os.path.join(pipelineDirectory, 'fib')
+sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
+sifFile = os.path.join(sifDirectory, 'dsistudio_latest.sif')
+singularityCommand = f'singularity exec {sifFile}'
 
 try:
     os.mkdir(figuresOutput)
@@ -10,8 +13,11 @@ except Exception as e:
     pass
 
 for subjectSession in os.listdir(fibDirectory):
+    thisdir = os.path.join(fibDirectory, subjectSession)
+    fname = os.listdir(thisdir)[0]
+    fibFile = os.path.join(thisdir, fname)
+    exportCommand = f'dsi_studio --action=exp --source={fibFile} --export=qa'
     
-    
-    
-    
-    pass
+    fullCommand = f'{singularityCommand} {exportCommand}'
+    print(f'{fullCommand}')
+    os.system(fullCommand)
