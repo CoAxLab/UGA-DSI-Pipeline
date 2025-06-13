463270f5ac3493388a46367bd1658bd85a2b0a79
peter <kasatyr@gmail.com>
2025-06-13 08:46:27 -0400

    reads anat metrics and makes distribution for snr totals

--- qualityDistributions.py
+++ qualityDistributions.py
@@ -1,8 +1,11 @@
 import os
+import json
+import matplotlib.pyplot as plt
 
 pipelineDirectory = os.getcwd()
 figuresOutput = os.path.join(pipelineDirectory, 'Figures')
 fibDirectory = os.path.join(pipelineDirectory, 'fib')
+qcOutputDirectory = os.path.join(pipelineDirectory, 'QCOutput')
 sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
 sifFile = os.path.join(sifDirectory, 'dsistudio_latest.sif')
 singularityCommand = f'singularity exec {sifFile}'
@@ -20,4 +23,20 @@ for subjectSession in os.listdir(fibDirectory):
     
     fullCommand = f'{singularityCommand} {exportCommand}'
     print(f'{fullCommand}')
-    os.system(fullCommand)
+    #os.system(fullCommand)
+
+snrTotals = []
+for sub in os.listdir(qcOutputDirectory):
+    currSub = os.path.join(qcOutputDirectory, sub)
+    if os.path.isdir(currSub) == False: continue
+    for ses in os.listdir(currSub):
+        if 'figures' in ses: continue
+        currSesAnat = os.path.join(currSub, ses, 'anat')
+        for file in os.listdir(currSesAnat):
+            if '.json' not in file: continue
+            jsonPath = os.path.join(currSesAnat, file)
+            f = open(jsonPath, 'r')
+            metrics = json.load(f)
+            snrTotals.append(metrics["snr_total"])
+plt.hist(snrTotals)
+plt.savefig(os.path.join(figuresOutput, 'snrTotalDistribution.png'))
