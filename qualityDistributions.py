96618068a5119cef763991fe8e9aac09c7182640
peter <kasatyr@gmail.com>
2025-06-13 10:34:02 -0400

    should create list of fd means. now requires nibabel

--- qualityDistributions.py
+++ qualityDistributions.py
@@ -1,6 +1,8 @@
 import os
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+import nibabel as nib
 
 pipelineDirectory = os.getcwd()
 figuresOutput = os.path.join(pipelineDirectory, 'Figures')
@@ -15,6 +17,7 @@ try:
 except Exception as e:
     pass
 
+allFDMeans = []
 for subjectSession in os.listdir(fibDirectory):
     thisdir = os.path.join(fibDirectory, subjectSession)
     fname = os.listdir(thisdir)[0]
@@ -24,6 +27,22 @@ for subjectSession in os.listdir(fibDirectory):
     fullCommand = f'{singularityCommand} {exportCommand}'
     print(f'{fullCommand}')
     os.system(fullCommand)
+
+    for file in os.listdir(thisdir):
+        if 'qa' not in file: continue
+        qaZipPath = os.path.join(thisdir, file)
+        qaFName = os.listdir(qaZipPath)[0]
+        qaFile = os.path.join(qaZipPath, qaFName)
+
+        object = nib.load(qaFile)
+        qaData = object.get_fdata()
+
+        if qaData.size == 1:
+            fdMean = float(qaData)
+            allFDMeans.append(fdMean)
+        else:
+            print(f'NEEDFIX')
+print(allFDMeans)
 
 snrTotals = []
 for sub in os.listdir(qcOutputDirectory):
