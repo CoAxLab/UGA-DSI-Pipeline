0052ab361cfd24490d1bec6dfd59301c84ed221a
Peter <kasatyr@gmail.com>
2025-07-18 15:14:57 -0400

    reports context

--- QCandRECON.py
+++ QCandRECON.py
@@ -15,9 +15,13 @@ qcOut = os.path.join(pipelineDirectory, 'QCOutput')
 if not os.path.isdir(qcOut):
     print(f'Attempting to run with "python3" command')
     os.system('python3 runQC.py')
+    print(f'*****\nQC complete\n*****\nStarting lowB toggle script\n*****')
     os.system('python3 addLowBToBIDS.py')
+    print(f'*****\nlowB move complete\n*****\nStarting DSI Studio pipeline\n*****')
     os.system('python3 runPipeline.py')
+    print(f'*****\nDSI Studio actions complete\n*****\nStarting Quality Distributions\n*****')
     os.system('python3 qualityDistributions.py')
+    print(f'*****\nQuality distributions complete!\n*****')
 
 if not os.path.isdir(qcOut):
     raise Exception(f'ERROR: Pipeline has not run, missing dependencies or Python not on path as "python" or "python3"')
