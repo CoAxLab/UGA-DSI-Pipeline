54f0e1bd4a4db261e2cf634b9984affa8aa23349
Peter <kasatyr@gmail.com>
2025-07-18 14:58:19 -0400

    updating master script

--- QCandRECON.py
+++ QCandRECON.py
@@ -6,14 +6,18 @@ if not os.path.isdir(bidsDirectory):
     raise Exception(f'ERROR: Please run the following:\n\tsetupPipeline.py, move data to convertToBids, and run niftiToBids.py\nbefore attempting to run the pipeline!')
 
 os.system('python runQC.py')
+os.system('python addLowBToBIDS.py')
 os.system('python runPipeline.py')
+os.system('python qualityDistributions.py')
 
 qcOut = os.path.join(pipelineDirectory, 'QCOutput')
 
 if not os.path.isdir(qcOut):
     print(f'Attempting to run with "python3" command')
     os.system('python3 runQC.py')
+    os.system('python3 addLowBToBIDS.py')
     os.system('python3 runPipeline.py')
+    os.system('python3 qualityDistributions.py')
 
 if not os.path.isdir(qcOut):
     raise Exception(f'ERROR: Pipeline has not run, missing dependencies or Python not on path as "python" or "python3"')
