d86d2df3307af0f7ebab3a247672e8280b027e76
Peter <kasatyr@gmail.com>
2025-07-18 15:22:40 -0400

    context fix, and condensed

--- QCandRECON.py
+++ QCandRECON.py
@@ -5,23 +5,21 @@ bidsDirectory = os.path.join(pipelineDirectory, 'BIDS')
 if not os.path.isdir(bidsDirectory):
     raise Exception(f'ERROR: Please run the following:\n\tsetupPipeline.py, move data to convertToBids, and run niftiToBids.py\nbefore attempting to run the pipeline!')
 
-os.system('python runQC.py')
-os.system('python addLowBToBIDS.py')
-os.system('python runPipeline.py')
-os.system('python qualityDistributions.py')
+def runScripts(python):
+    os.system(f'{python} runQC.py')
+    print(f'*****\nQC complete\n*****\nStarting lowB toggle script\n*****')
+    os.system(f'{python} addLowBToBIDS.py')
+    print(f'*****\nlowB move complete\n*****\nStarting DSI Studio pipeline\n*****')
+    os.system(f'{python} runPipeline.py')
+    print(f'*****\nDSI Studio actions complete\n*****\nStarting Quality Distributions\n*****')
+    os.system(f'{python} qualityDistributions.py')
+    print(f'*****\nQuality distributions complete!\n*****')
 
+runScripts('python')
 qcOut = os.path.join(pipelineDirectory, 'QCOutput')
-
 if not os.path.isdir(qcOut):
     print(f'Attempting to run with "python3" command')
-    os.system('python3 runQC.py')
-    print(f'*****\nQC complete\n*****\nStarting lowB toggle script\n*****')
-    os.system('python3 addLowBToBIDS.py')
-    print(f'*****\nlowB move complete\n*****\nStarting DSI Studio pipeline\n*****')
-    os.system('python3 runPipeline.py')
-    print(f'*****\nDSI Studio actions complete\n*****\nStarting Quality Distributions\n*****')
-    os.system('python3 qualityDistributions.py')
-    print(f'*****\nQuality distributions complete!\n*****')
+    runScripts('python3')
 
 if not os.path.isdir(qcOut):
     raise Exception(f'ERROR: Pipeline has not run, missing dependencies or Python not on path as "python" or "python3"')
