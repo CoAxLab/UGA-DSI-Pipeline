39e58708b6cde2690feb8caa6863e213314e08d4
Peter <kasatyr@gmail.com>
2025-07-24 16:57:37 -0400

    broke out into two functions: create directories, update singularity images

--- Scripts/setupPipeline.py
+++ Scripts/setupPipeline.py
@@ -1,24 +1,34 @@
 import os
 
 pipelineDirectory = os.getcwd()
-
-# create directories
 sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
 sourceDirectoryDCM = os.path.join(pipelineDirectory, 'convertToBids')
 outputDirectorySRC = os.path.join(pipelineDirectory, 'src')
 outputDirectoryFIB = os.path.join(pipelineDirectory, 'fib')
-for path in [sifDirectory, sourceDirectoryDCM, outputDirectorySRC, outputDirectoryFIB]:
-    try:
-        os.mkdir(path)
-        print(f'\nCreated directory at: {path}!')
-    except FileExistsError:
-        print(f'\nFile path: {path} already exists!')
 
-# pull SIF file for dsi studio
-os.chdir(sifDirectory)
-os.system('singularity pull docker://dsistudio/dsistudio:latest')
-os.system('singularity pull docker://nipreps/mriqc:latest')
-os.chdir(pipelineDirectory)
+def CreateDirs() -> None:
+    # create directories
+    for path in [sifDirectory, sourceDirectoryDCM, outputDirectorySRC, outputDirectoryFIB]:
+        try:
+            os.mkdir(path)
+            print(f'\nCreated directory at: {path}!')
+        except FileExistsError:
+            print(f'\nFile path: {path} already exists!')
 
-print(f'\n\nSet-Up complete!')
-print(f'\nPlease move participant data directories to:\n     {sourceDirectoryDCM}')
+def UpdateImages() -> None:
+    # pull SIF file for dsi studio
+    os.chdir(sifDirectory)
+    os.system('singularity pull docker://dsistudio/dsistudio:latest')
+    os.system('singularity pull docker://nipreps/mriqc:latest')
+    os.chdir(pipelineDirectory)
+
+
+
+def main() -> None:
+    CreateDirs()
+    UpdateImages()
+    print(f'\n\nSet-Up complete!')
+    print(f'\nPlease move participant data directories to:\n     {sourceDirectoryDCM}')
+
+if __name__ == "__main__":
+    main()
