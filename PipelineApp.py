a4bd4ee572cb6dc19fc65f6c18b59b09bc89a40e
Peter <kasatyr@gmail.com>
2025-07-24 17:37:30 -0400

    nifti button

--- PipelineApp.py
+++ PipelineApp.py
@@ -1,10 +1,18 @@
 import sys
 import os
+import Scripts
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QPushButton, QMessageBox
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 
+import Scripts.niftiToBids
+import Scripts.setupPipeline
+
 class MainWindow(QMainWindow):
+    # class DSIButton(QPushButton):
+    #     def __init__(self, action:function):
+    #         super().__init__()
+    #         self.action = action
     def __init__(self):
         super().__init__()
         self.setWindowTitle(f"DSI Studio Pipeline")
@@ -27,18 +35,31 @@ class MainWindow(QMainWindow):
         self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         layout.addWidget(self.label)
 
-        self.setupButton = QPushButton("Set up directories")
+        if os.path.isdir('convertToBids'):
+            # only refresh images if directory setup already is complete
+            self.setupButton = QPushButton("Pull Singularity images (create or update)")
+            self.setupAction = Scripts.setupPipeline.UpdateImages
+        else:
+            self.setupButton = QPushButton("Set up directories")
+            self.setupAction = Scripts.setupPipeline.main
         self.setupButton.clicked.connect(self.setupButtonClick)
         layout.addWidget(self.setupButton)
-        if os.path.isdir('convertToBids'):
-            # button will be inactive if setup does not need run again
-            self.makeInactive(self.setupButton)
+
+        self.niftiButton = QPushButton("Move nifti files to BIDS directory")
+        self.niftiButton.clicked.connect(self.niftiButtonClick)
+        layout.addWidget(self.niftiButton)
 
     def setupButtonClick(self):
         self.label.setText("Attempting to set up directories...")
-        os.system(f'python setupPipeline.py')
+        self.setupAction() # determined when button is initialized
         self.label.setText("Setup complete!")
         self.makeInactive(self.setupButton)
+    
+    def niftiButtonClick(self):
+        self.label.setText("Moving nifti files to BIDS format...")
+        Scripts.niftiToBids.NiftiToBIDS()
+        self.label.setText("BIDS re-format complete!")
+        self.makeInactive(self.niftiButton)
 
     def makeInactive(self, button:QPushButton):
         button.setDisabled(True)
