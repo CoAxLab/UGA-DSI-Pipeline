f795fab7ed475a7a7cabd2ec67d4a7bbad00f46b
Peter <kasatyr@gmail.com>
2025-07-24 16:44:12 -0400

    setup button version 1

--- PipelineApp.py
+++ PipelineApp.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+import os
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QPushButton, QMessageBox
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 
@@ -7,16 +8,44 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle(f"DSI Studio Pipeline")
-        self.setGeometry(300, 200, 1100, 600) # x, y, w, h
+        self.Xstart = 300
+        self.WIDTH = 1200
+        self.Ystart = 200
+        self.HEIGHT = 600
+        self.setGeometry(self.Xstart, self.Ystart, self.WIDTH, self.HEIGHT)
 
-        self.label = QLabel("Welcome3!", self)
-        self.label.setFont(QFont("Arial", 30))
-        self.label.setGeometry(0,0,200,50)
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
+        layout = QVBoxLayout(centralWidget)
+
+        self.label = QLabel("Welcome!", self)
+        self.label.setFont(QFont("Serif", 20))
         self.label.setStyleSheet(
             "color: #361212;"
             "font-weight: bold;"
             )
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(self.label)
+
+        self.setupButton = QPushButton("Set up directories")
+        self.setupButton.clicked.connect(self.setupButtonClick)
+        layout.addWidget(self.setupButton)
+        if os.path.isdir('convertToBids'):
+            # button will be inactive if setup does not need run again
+            self.makeInactive(self.setupButton)
+
+    def setupButtonClick(self):
+        self.label.setText("Attempting to set up directories...")
+        os.system(f'python setupPipeline.py')
+        self.label.setText("Setup complete!")
+        self.makeInactive(self.setupButton)
+
+    def makeInactive(self, button:QPushButton):
+        button.setDisabled(True)
+        button.setToolTip("Action already complete!")
+
+        
+
 
 def main():
     app = QApplication(sys.argv)
