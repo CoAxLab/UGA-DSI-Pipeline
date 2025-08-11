c4bf23959d7829aab4471299b90a23c11bc3f794
peter <kasatyr@gmail.com>
2025-08-11 14:53:38 -0400

    builds and returns lists

--- Scripts/Util/FetchFiles.py
+++ Scripts/Util/FetchFiles.py
@@ -3,19 +3,19 @@ import os
 pipelineDir = os.getcwd()
 figures = os.path.join(pipelineDir, 'Figures')
 
-def FetchFigures()->tuple[set, set, set]:
+def FetchFigures()->tuple[list, list, list]:
     '''
-    Returns tuple of sets that contain full file path for figures:
-        ({T1wFiles}, {T2wFiles}, {dwiFiles})
+    Returns tuple of lists that contain full file path for figures:
+        ([T1wFiles], [T2wFiles], [dwiFiles])
     '''
-    t1, t2, dwi = set(), set(), set()
+    t1, t2, dwi = [], [], []
     for f in os.listdir(figures):
         if '.png' in f:
             fPath = os.path.join(figures, f)
             if 'T1w' in f:
-                t1.add(fPath)
+                t1.append(fPath)
             elif 'T2w' in f:
-                t2.add(fPath)
+                t2.append(fPath)
             elif 'dwi' in f:
-                dwi.add(fPath)
+                dwi.append(fPath)
     return t1, t2, dwi
