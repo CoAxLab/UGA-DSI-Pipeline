b4d8a1356e5b0ffd38b169262cec1395734c4c8d
peter <kasatyr@gmail.com>
2026-02-05 12:33:37 -0500

    re-try

--- Scripts/runPipeline.py
+++ Scripts/runPipeline.py
@@ -88,14 +88,18 @@ def RunSRC()->None:
             bvalTargets = ''
             bvecTargets = ''
             for file in os.listdir(absNiftiPath):
+                if niftiTargets != '':
+                    niftiTargets = f'{niftiTargets}, '
+                    bvalTargets = f'{bvalTargets}, '
+                    bvecTargets = f'{bvecTargets}, '
                 if '.nii.gz' in file:
-                    niftiTargets = f'{niftiTargets}{os.path.join(niftiInDirectory, file)},'
+                    niftiTargets = f'{niftiTargets}{os.path.join(niftiInDirectory, file)}'
                 elif '.bval' in file:
-                    bvalTargets = f'{bvalTargets}{os.path.join(niftiInDirectory, file)},'
+                    bvalTargets = f'{bvalTargets}{os.path.join(niftiInDirectory, file)}'
                 elif '.bvec' in file:
-                    bvecTargets = f'{bvecTargets}{os.path.join(niftiInDirectory, file)},'
-            for s in [niftiTargets, bvalTargets, bvecTargets]:
-                s = s[:-1]
+                    bvecTargets = f'{bvecTargets}{os.path.join(niftiInDirectory, file)}'
+            # for s in [niftiTargets, bvalTargets, bvecTargets]:
+            #     s = s[:-1]
 
             srcCommandPart = f'dsi_studio --action=src --source={niftiTargets} --bval={bvalTargets} --bvec={bvecTargets} --output={singleSRCOutFile}'
             srcFullCommand = f'{singularityCommand} {srcCommandPart}'
