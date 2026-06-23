d7dae884c0aaf2857276feb4304980c646d14b14
Peter <kasatyr@gmail.com>
2026-06-23 16:05:29 -0400

    removed lines involving SIF setup

--- Scripts/setupPipeline.py
+++ Scripts/setupPipeline.py
@@ -2,7 +2,7 @@ import os
 from datetime import date
 
 pipelineDirectory = os.getcwd()
-sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
+#sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
 sourceDirectoryDCM = os.path.join(pipelineDirectory, 'convertToBids')
 outputDirectorySRC = os.path.join(pipelineDirectory, 'src')
 outputDirectoryFIB = os.path.join(pipelineDirectory, 'fib')
@@ -10,27 +10,27 @@ ymd = date.today().strftime('%Y-%m-%d')
 
 def CreateDirs() -> None:
     # create directories
-    for path in [sifDirectory, sourceDirectoryDCM, outputDirectorySRC, outputDirectoryFIB]:
+    for path in [sourceDirectoryDCM, outputDirectorySRC, outputDirectoryFIB]:
         try:
             os.mkdir(path)
             print(f'\nCreated directory at: {path}!')
         except FileExistsError:
             print(f'\nFile path: {path} already exists!')
 
-def UpdateImages() -> None:
-    # pull SIF file for dsi studio
-    #os.chdir(sifDirectory)
-    dsiName = os.path.join(sifDirectory, f'dsistudio_{ymd}')
-    qcName = os.path.join(sifDirectory, f'mriqc_{ymd}')
-    os.system(f'singularity pull --name {dsiName} docker://dsistudio/dsistudio:latest')
-    os.system(f'singularity pull --name {qcName} docker://nipreps/mriqc:latest')
-    #os.chdir(pipelineDirectory)
+# def UpdateImages() -> None:
+#     # pull SIF file for dsi studio
+#     #os.chdir(sifDirectory)
+#     dsiName = os.path.join(sifDirectory, f'dsistudio_{ymd}')
+#     qcName = os.path.join(sifDirectory, f'mriqc_{ymd}')
+#     os.system(f'singularity pull --name {dsiName} docker://dsistudio/dsistudio:latest')
+#     os.system(f'singularity pull --name {qcName} docker://nipreps/mriqc:latest')
+#     #os.chdir(pipelineDirectory)
 
 
 
 def main() -> None:
     CreateDirs()
-    UpdateImages()
+    #UpdateImages()
     print(f'\n\nSet-Up complete!')
     print(f'\nPlease move participant data directories to:\n     {sourceDirectoryDCM}')
 
